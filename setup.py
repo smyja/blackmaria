6ce6663a310daa425997027406f1d6d27875511a
Smyja <akposlive59@gmail.com>
2023-03-26 20:10:46 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
     description = fh.read()
 setup(
     name=NAME,
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     url= URL,
     license='MIT',
